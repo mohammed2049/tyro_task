ebca71893b9598dd7f22566c03c48ee0a6ade954
mohammed2049 <mohammed.shane94@gmail.com>
2018-09-29 00:26:56 +0200

    edit routes and method name

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -5,13 +5,12 @@ class FeedbacksController < ApplicationController
   # GET /feedbacks
   def index
     @feedbacks = apply_scopes(Feedback)
-
+    
     render json: @feedbacks
   end
 
-  # GET /feedbacks/1
-  def show
-    feedback = apply_scopes(Feedback)
+  def show_feedback
+    feedback = Feedback.find_by(number: params[:number], company_token: params[:company_token])
     render json: feedback
   end
 
@@ -22,22 +21,24 @@ class FeedbacksController < ApplicationController
     if @feedback.valid?
       FeedbackJob.perform_later(@feedback.attributes, @feedback.state.attributes)
       number = Rails.cache.read(@feedback.company_token) || 0
-      render json: {number: number + 1}, status: :created
+      render json: { number: number + 1 }, status: :created
     else
       render json: @feedback.errors, status: :unprocessable_entity
     end
   end
+
   # GET /feedbacks/count
   def count
-    render json: Rails.cache.fetch(params[:company_token]) { apply_scopes(Feedback).count } , status: 200
+    render json: Rails.cache.fetch(params[:company_token]) { apply_scopes(Feedback).count }, status: 200
   end
+
   private
 
-    def feedback_params
-      params.require(:feedback).permit(:company_token, :number, :priority)
-    end
+  def feedback_params
+    params.require(:feedback).permit(:company_token, :number, :priority)
+  end
 
-    def state_params
-      params.require(:state).permit(:device, :os, :memory, :storage)
-    end
+  def state_params
+    params.require(:state).permit(:device, :os, :memory, :storage)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  resources :feedbacks, param: :number, only: %i[index show create] do
+  resources :feedbacks, only: %i[index create] do
     collection do
       get :count
+      get '/:number', to: 'feedbacks#show_feedback'
     end
   end
 end
