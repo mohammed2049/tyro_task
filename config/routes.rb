a85e17c206aaa311a3bc9ee1976783cb97dca630
mohammed2049 <mohammed.shane94@gmail.com>
2018-09-28 22:24:24 +0200

    use sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
+  resources :feedbacks, param: :number, only: %i[index show create] do
+    collection do
+      get :count
+    end
+  end
 end
